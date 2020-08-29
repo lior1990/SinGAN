44eb304f9087c3ea733a1d91952cbb8be8423f00
unknown <lior1990@gmail.com>
2020-08-29 22:12:03 +0300

    modify multiple main train

--- multiple_main_train.py
+++ multiple_main_train.py
@@ -3,16 +3,25 @@ from argparse import Namespace
 from main_train import main, parse_arguments
 
 
-IMAGES = [("birds1.jpg", "birds2.jpg"), ("birds.png", "balloons_size_birds.png")]
+IMAGES = [("birds.png", "balloons_size_birds.png"), ("birds1.jpg", "birds2.jpg")]
 
-CONFIGURATIONS = {"scale_factor": [0.8],
-                  "mixed_imgs_training": [False],
-                  "noise_vectors_merge_method": ["sum"],
-                  "gaussian_noise_z_distance": [0],
-                  "alpha": [15],
-                  "replace_background": [True],
-                  "nfc": [128, 32]
+CONFIGURATIONS = {
+                  "mask_activation_fn": ["sigmoid", "relu_sign", "tanh_relu"],
+                  "cyclic_lr": [False, True],
                   }
+DEFAULT_PARAMS = {
+    "scale_factor": 0.8,
+    "mixed_imgs_training": False,
+    "noise_vectors_merge_method": "sum",
+    "gaussian_noise_z_distance": 0,
+    "alpha": 40,
+    "replace_background": True,
+    "nfc": 64,
+    "min_nfc": 64,
+    "weight_decay_d_mask1": 0.1,
+    "weight_decay_d_mask2": 0.1,
+    "num_layer": 6,
+}
 
 
 def _generate_opt(configuration: dict):
@@ -23,28 +32,17 @@ def _generate_opt(configuration: dict):
 def multiple_main():
 
     for img1, img2 in IMAGES:
-        for scale_factor in CONFIGURATIONS["scale_factor"]:
-            for mixed_imgs_training in CONFIGURATIONS["mixed_imgs_training"]:
-                for noise_vectors_merge_method in CONFIGURATIONS["noise_vectors_merge_method"]:
-                    for gaussian_noise_z_distance in CONFIGURATIONS["gaussian_noise_z_distance"]:
-                        for alpha in CONFIGURATIONS["alpha"]:
-                            for replace_background in CONFIGURATIONS["replace_background"]:
-                                for nfc in CONFIGURATIONS["nfc"]:
-                                    mixed_str = "mixed" if mixed_imgs_training else ""
-                                    exp_name = f"{mixed_str}_{noise_vectors_merge_method}_g-dist{gaussian_noise_z_distance}_alpha{alpha}_nfc{nfc}"
-                                    config = {
-                                        "scale_factor": scale_factor,
-                                        "mixed_imgs_training": mixed_imgs_training,
-                                        "noise_vectors_merge_method": noise_vectors_merge_method,
-                                        "gaussian_noise_z_distance": gaussian_noise_z_distance,
-                                        "alpha": alpha,
-                                        "exp_name": exp_name,
-                                        "replace_background": replace_background,
-                                        "nfc": nfc,
-                                        "min_nfc": nfc,
-                                    }
-                                    _run_with_images(config, exp_name, img1, img2)
-                                    _run_with_images(config, exp_name, img2, img1)
+        for mask_activation_fn in CONFIGURATIONS["mask_activation_fn"]:
+            for cyclic_lr in CONFIGURATIONS["cyclic_lr"]:
+                exp_name = f"{mask_activation_fn}_{cyclic_lr}"
+                config = {
+                    "cyclic_lr": cyclic_lr,
+                    "mask_activation_fn": mask_activation_fn,
+                    "exp_name": exp_name,
+                }
+                config.update(DEFAULT_PARAMS)
+                # _run_with_images(config, exp_name, img1, img2)
+                _run_with_images(config, exp_name, img2, img1)
 
 
 def _run_with_images(config, exp_name, img1, img2):
